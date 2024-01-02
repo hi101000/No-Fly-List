2d48975fd3d5175a2bd8b4d0990a4292a0d748fb
hi101000 <60079731+hi101000@users.noreply.github.com>
2024-01-02 18:27:39 -0500

    Delete pre-processed directory

--- pre-processed/unzip.py
+++
@@ -1,8 +0,0 @@
-# importing the zipfile module 
-from zipfile import ZipFile 
-from os import system
-
-# loading the temp.zip and creating a zip object 
-with ZipFile("NOFLY.csv.zip", 'r') as zObject: 
-    zObject.extractall( path="NOFLY.csv")
-    system('rm -f NOFLY.csv.zip')
